58796ca304d8404d465292bf3116d97d7e304ee4
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2018-01-03 10:00:26 +0200

    add webpack config for standalone build

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin({configFile:"./tsconfig.json"})]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+    ]
+  }
+}
