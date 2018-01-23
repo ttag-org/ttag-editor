b2dafe7a33cb0e8c6b830c2a1eafdf1b9a055fdf
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2018-01-23 23:55:46 +0200

    added savebtn

---
+++ src/components/Base/SaveBtn.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { PoData } from "src/lib/parser";
+import { serialize } from "src/lib/serializer";
+import RaisedButton from "material-ui/RaisedButton";
+
+type SaveBtnProps = {
+  poFile: PoData;
+};
+
+function saveTranslations(poData: PoData) {
+  const content = serialize(poData);
+  fetch('/save', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+        'Content-Length': content.length.toString(),
+      },
+      body: content
+  });
+}
+
+export const SaveBtn = (props: SaveBtnProps) => {
+  return (
+    <RaisedButton
+      secondary={true}
+      label="Save"
+      onClick={() => saveTranslations(props.poFile)}
+    />
+  );
+};
